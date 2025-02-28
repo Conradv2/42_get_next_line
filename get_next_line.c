97fb5f844422ca77c3f51ae2b6bda069a8d2e24c
Conrad <marek226@interia.pl>
2025-02-28 17:59:42 +0100

    i think it is on the right track xdd

--- get_next_line.c
+++ get_next_line.c
@@ -16,41 +16,28 @@ char	*get_next_line(int fd)
 {
 	char		*buff;
 	char		*line;
+	char		*tmp;
 	static char	*left_str;
 	int			bytesread;
-	char		*new_position;
 
 	if (fd <= 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	bytesread = read(fd, buff, BUFFER_SIZE);
-	if (bytesread <= 0)
+	while((bytesread = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
-		free(buff);
-		return (NULL);
-	}
-	buff[bytesread] = '\0';
-	if (left_str == NULL)
-		line = ft_strdup(buff);
-	else
-	{
-		line = ft_strjoin(left_str, buff);
-		free(left_str);
-	}
-	new_position = ft_strchr(line,'\n');
-	if (new_position != NULL)
-	{
-		*new_position = '\0';
-		line = ft_strdup(left_str);
-		left_str = ft_strdup(new_position + 1);
-	}
-	else
-	{
-		line = ft_strdup(left_str);
-		free(left_str);
-		left_str = NULL;
+		buff[bytesread] = '\0';
+		if (left_str == NULL)
+			left_str = ft_strdup(buff);
+		else
+		{
+			tmp = ft_strjoin(left_str, buff);
+			free(left_str);
+			left_str = tmp;
+		}
+		if (ft_strchr(buff, '\n') != NULL )
+			break;
 	}
 	return (line);
 }
