10ac60b767d52ff471fa1bf0de49ff50b5a924af
Conrad <marek226@interia.pl>
2025-02-28 04:10:18 +0100

    added new function ft_strchr

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: conrad <conrad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 13:43:16 by conrad            #+#    #+#             */
-/*   Updated: 2025/02/26 20:51:51 by conrad           ###   ########.fr       */
+/*   Updated: 2025/02/28 03:54:50 by conrad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup( const char *str1 );
+char	*ft_strchr(const char *str, int search_str);
 size_t	ft_strlen(const char*str);
 void	ft_putstr_fd(char *s, int fd);
 void	*ft_calloc(size_t nitems, size_t size);
